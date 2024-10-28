044485b1a407bc8ac70bc5382e2783b7806e1ba7
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-10-28 12:18:22 +0600

    java

--- function.js
+++ function.js
@@ -39,7 +39,7 @@ console.log(now.toLocaleString());
 var posVal = 44;
 var negVal = -posVal;
 var diffVal = posVal - negVal;
-// console.log(negVal);
+console.log(negVal);
 
 const arr = [4, 6, 7, 52, 21, 2];
 let large = arr[0];
@@ -55,8 +55,8 @@ for (i = 0; i < arr.length; i++) {
   sum += arr[i];
 }
 console.log(small);
-// console.log(large);
-// console.log(sum);
+console.log(large);
+console.log(sum);
 
 const array1 = [41, 0, -1, -3, 5, -4, -7, 0, 9, 0, 786, -98, 6];
 let positivecounte = 0;
@@ -72,12 +72,12 @@ for (let i = 0; i < array1.length; i++) {
     zerocounte++;
   }
   add += array1[i];
-} /*
+}
 console.log(positivecounte);
 console.log(negitivecounte);
 console.log(zerocounte);
 console.log(add);
-*/
+
 // ascil patta
 let p = parseInt(prompt("enter any number:"));
 for (let i = 0; i <= p; i++) {
