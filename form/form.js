736d6b819736b7a49ed65afbaf82b1f7168be4ff
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-03 12:18:55 +0600

    java

--- form/form.js
+++ form/form.js
@@ -4,6 +4,13 @@ function myform() {
   var add = document.f.add.value;
   var geander = document.f.Gender.value;
   var round = document.f.round.value;
+  // var skill = document.f.skill.value;
+  var skill = "";
+  for (i = 0; i < document.f.skill.length; i++) {
+    if (document.f.skill[i].checked) {
+      skill += document.f.skill[i].value + ",";
+    }
+  }
   doc = open("", "", "width=300px, height=400px ,top=50px,left=100px");
   with (doc.document) {
     write("Name: " + nam + "<br>");
@@ -11,6 +18,7 @@ function myform() {
     write("Address: " + add + "<br>");
     write("Gender: " + geander + "<br>");
     write("Round: " + round + "<br>");
+    write("Skill: " + skill + "<br>");
     write("<input type='button' value='Close me' onclick='self.close()'>");
     write("<input type='button' value='Print me' onclick='self.print()'>");
   }
