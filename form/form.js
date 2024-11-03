bdb4367c5f1cd5c80f80391bc860c7b448be698f
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-03 13:07:46 +0600

    java

--- form/form.js
+++ form/form.js
@@ -11,15 +11,25 @@ function myform() {
       skill += document.f.skill[i].value + ",";
     }
   }
-  doc = open("", "", "width=300px, height=400px ,top=50px,left=100px");
+  doc = open("", "", "width=400px, height=400px ,top=50px,left=100px");
   with (doc.document) {
+    write("<html>");
+    write("<head><title>" + title + "</title></head>");
+    write("<body style='background-color:teal;color:white;padding:50px'>");
+    write("<section style='border:1px solid gray;padding:20px'><section>");
     write("Name: " + nam + "<br>");
     write("Email: " + emil + "<br>");
     write("Address: " + add + "<br>");
     write("Gender: " + geander + "<br>");
     write("Round: " + round + "<br>");
     write("Skill: " + skill + "<br>");
-    write("<input type='button' value='Close me' onclick='self.close()'>");
-    write("<input type='button' value='Print me' onclick='self.print()'>");
+    write(
+      "<input style='background-color:red;color:white;outline:none;border:none' type='button' value='Close me' onclick='self.close()'>"
+    );
+    write(
+      "<input style='background-color:skyblue;color:white;outline:none; border:none' type='button' value='Print me' onclick='self.print()'>"
+    );
+    write("</body");
+    write("</html>");
   }
 }
